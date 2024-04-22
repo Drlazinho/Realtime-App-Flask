470c15ab51f48a2f881f310e7933540c71116da6
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-04-22 06:10:11 -0300

    Criando o Pagamento Pix com QR Code

---
+++ payments/pix.py
@@ -0,0 +1,23 @@
+import qrcode
+import uuid
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        # criar o pagamento na instituicao financeira
+        bank_payment_id = uuid.uuid4()
+
+        # codigo copia e cola
+        hash_payment = f'hash_payement_{bank_payment_id}'
+        
+        # qr code -> gerar img
+        img = qrcode.make(hash_payment)
+        # salvar a img
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}",
+        }
